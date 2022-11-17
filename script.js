0bf1e3232834a908b9fd78f13bf840700a6fd18a
IvanMartinezDev <martinezeliasivanb@gmail.com>
2022-11-17 17:10:43 -0300

    "first commit"

---
+++ script.js
@@ -0,0 +1,11 @@
+var pais = "";
+var continente = "";
+var antiguedad = "2022";
+
+pais = "Mexico";
+continente = "Latinoamerica";
+
+var pais_y_continente = pais + " " + continente;
+
+console.log(pais, continente, antiguedad);
+alert(pais_y_continente);
